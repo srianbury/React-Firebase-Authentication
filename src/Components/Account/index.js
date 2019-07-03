5b83246e69607bad845007491eeef9d21a6e38f0
Brian Sunbury <bsunbury29@gmail.com>
2019-07-02 19:38:24 -0700

    password change
    added passwod change and added that and password reset to the Account page

--- src/Components/Account/index.js
+++ src/Components/Account/index.js
@@ -1,5 +1,16 @@
 import React from 'react';
 
-const Account = () => <div>Account</div>;
+import { PasswordForgetForm } from '../PasswordForget';
+import PasswordChangeForm from '../PasswordChange';
 
-export default Account;
+
+const AccountPage = () => (
+    <div>
+        <h1>Account Page</h1>
+        <PasswordForgetForm />
+        <PasswordChangeForm />
+    </div>
+);
+
+
+export default AccountPage;
--- src/Components/PasswordChange/index.js
+++ src/Components/PasswordChange/index.js
@@ -1,5 +1,55 @@
 import React from 'react';
 
-const PasswordChange = () => <div>PasswordChange</div>;
+import { withFirebase } from '../Firebase';
+import { useForm } from '../../Hooks';
 
-export default PasswordChange;
+const PasswordChangeForm = (props) => {
+    const [formData, dispatch, reset] = useForm(initialForm);
+    function handleSubmit(){
+        const { passwordOne } = formData;
+
+        props.firebase
+            .doPasswordUpdate(passwordOne)
+            .then(() => {
+                reset();
+            })
+            .catch(error => {
+                dispatch({ error });
+            });    
+    };
+
+    function handleChange(event){
+        dispatch({ [event.target.name]: event.target.value });
+    }
+
+    return(
+        <div>
+            {formData.error && <p>{formData.error.message}</p>}
+            <input
+                name="passwordOne"
+                value={formData.passwordOne}
+                onChange={handleChange}
+                type="password"
+                placeholder="New Password" />
+            <input
+                name="passwordTwo"
+                value={formData.passwordTwo}
+                onChange={handleChange}
+                type="password"
+                placeholder="Confirm New Password" />
+            <button 
+                onClick={handleSubmit}
+                disabled={!formData.passwordOne || formData.passwordOne!==formData.passwordTwo}>
+                Change Password
+            </button>
+        </div>
+    );
+};
+const initialForm = {
+    passwordOne: '',
+    passwordTwo: '',
+    error: null,
+};
+
+
+export default withFirebase(PasswordChangeForm);
